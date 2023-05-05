0b85acc9c53bf94bce8212ae7a313f6dcf376b30
Giovanna Mendonça Federico <106484465+giih06@users.noreply.github.com>
2023-05-05 08:18:54 -0300

    anotações aula

--- src/entities/Executor2.java
+++ src/entities/Executor2.java
@@ -1,5 +1,15 @@
 package entities;
 
 public class Executor2 {
-    
+    public static void main(String[] args) throws Exception {
+        String paUsuario = palavraDoUsuario();
+        String paOculta = JogoAdivinhacao.palavraOculta();
+
+        if(paUsuario.equals(paOculta)) {
+            Impressao.imprimir("Acertou");
+        } else {
+            Impressao.imprimir("Errou");
+        }
+
+    }
 }
