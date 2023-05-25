a256a4583041d2d17c1b012fa3606b92b25e3ede
Geraldo Alencar <geraldoalencardev@gmail.com>
2023-05-25 20:44:01 -0300

    criação do menu
    criação do menu e dos cases.

--- src/application/Executor.java
+++ src/application/Executor.java
@@ -1,12 +1,40 @@
 package application;
 import entities.Impressao;
 import entities.JogoDaForca;
+import java.util.Scanner;
 
 public class Executor {
     // Executor
     public static void main(String[] args) throws Exception {
-        JogoDaForca forca = new JogoDaForca();
-        Impressao.imprimir(forca);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------------------------------------------");
+		System.out.println("|               MENU DE JOGOS!!!            |");
+		System.out.println("---------------------------------------------");
+		System.out.println("1- Sudoku  2- Jogo Da Forca  3- Jogo Da Velha");
+		System.out.println("---------------------------------------------");
+		System.out.print("Informe o numero do jogo desejado: ");
+		int jogoEscolhido = sc.nextInt();
+		System.out.println("---------------------------------------------");
+		
+		
+		switch (jogoEscolhido) {
+		case 1:
+			System.out.println("Sudoku");
+			break;
+
+		case 2:
+			System.out.println("Jogo da Forca");
+			break;
+		
+		case 3:
+			System.out.println("Jogo da Velha");
+			break;
+			
+		default:
+			
+			break;
+			
+		}
 
     }
 }
